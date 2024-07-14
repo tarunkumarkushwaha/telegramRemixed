bbc82e4096133716962bc56d8e2433e238df22dd
tarunkumarkushwaha <tkk.tarunkumar02@gmail.com>
2024-07-14 23:35:09 +0530

    wave animation fix 1

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -17,11 +17,11 @@ const SideBar = ({ Showsidebar, toggleNav, dark, setdark }) => {
     }, [Showsidebar, toggleNav]);
 
     const toggleTheme = () => {
-        setAnimateEffect(true); 
+        setAnimateEffect(true);
         setTimeout(() => {
-            setdark(!dark); 
-            setAnimateEffect(false); 
-        }, 500); 
+            setdark(!dark);
+            setAnimateEffect(false);
+        }, 500);
     };
 
     useEffect(() => {
@@ -34,14 +34,14 @@ const SideBar = ({ Showsidebar, toggleNav, dark, setdark }) => {
     return (
         <>
             <aside ref={menuRef} className={`fixed z-20 top-0 left-0 h-full transition-all duration-1000 ease-in-out ${!Showsidebar ? "md:w-[25vw] w-[100vw] -translate-x-full" : "md:w-[30vw] w-[50vw] -translate-x-1"}`}>
+                {animateEffect && <div className={`animate-wave ${!dark ? "bg-zinc-900/95 text-slate-50" : "bg-zinc-50/95 text-slate-900"}`}></div>}
                 <div className={`h-full px-3 py-4 overflow-y-auto ${dark ? "bg-zinc-900/95 text-slate-50" : "bg-zinc-50/95 text-slate-900"}`}>
                     <div onClick={toggleNav} className={`cursor-pointer pl-10 w-4 text-xxl fixed top-8 right-[35px] `}>
                         {Showsidebar && <CloseIcon />}
                     </div>
                     <div onClick={toggleTheme} className={`border ${dark ? "border-white" : "border-black"} rounded-full p-1 cursor-pointer text-xxl absolute top-8 right-[75px] `}>
-                        {dark ? <DarkModeIcon/> : <WbSunnyIcon />}
+                        {dark ? <DarkModeIcon /> : <WbSunnyIcon />}
                     </div>
-                    {animateEffect && <div className={`animate-wave ${!dark ? "bg-zinc-900/95 text-slate-50" : "bg-zinc-50/95 text-slate-900"}`}></div>}
                     <ul className="space-y-2 font-medium">
                         <li>
                             <div className="flex justify-center items-center">
